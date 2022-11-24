bd855ede02ef334e6c3e942636b48d429124fe2b
joneswong <jones_wong@icloud.com>
2022-11-24 13:24:49 +0000

    validate ready

---
+++ db/migrate/20221124120237_create_lists.rb
@@ -0,0 +1,9 @@
+class CreateLists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :lists do |t|
+      t.string :name
+      t.references :bookmark, foreign_key: true
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20221124120256_create_bookmarks.rb
@@ -0,0 +1,9 @@
+class CreateBookmarks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookmarks do |t|
+      t.string :comment
+      t.references :movie, :list, foreign_key: true
+      t.timestamps
+    end
+  end
+end
